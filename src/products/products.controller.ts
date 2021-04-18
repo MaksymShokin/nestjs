04e470379d3aa640e0ebaaa8fc9bcc8382141845
Maksym <maksym.sh@upplabs.com>
2021-04-18 21:15:06 +0300

    products-controller

---
+++ src/products/products.controller.ts
@@ -0,0 +1,22 @@
+import { ProductsService } from './products.service';
+import { Controller, Post, Body } from '@nestjs/common';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  addProduct(
+    @Body('title') prodTitle: string,
+    @Body('description') prodDescription: string,
+    @Body('price') prodPrice: number,
+  ): any {
+    const generatedId = this.productsService.insertProduct(
+      prodTitle,
+      prodDescription,
+      prodPrice,
+    );
+
+    return { id: generatedId };
+  }
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,21 @@
+import { Product } from './product.model';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ProductsService {
+  products: Product[] = [];
+
+  insertProduct(title: string, description: string, price: number) {
+    const prodId = new Date().toString();
+    const newProduct = new Product(
+      new Date().toString(),
+      title,
+      description,
+      price,
+    );
+
+    this.products.push(newProduct);
+
+    return prodId;
+  }
+}
